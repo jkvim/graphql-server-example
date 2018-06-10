41e46e29ee8d4bee36daf7660809093ea0dfce47
Lin Zehang <zhlin@thoughtworks.com>
2018-06-10 21:30:12 +0800

    add custom scalar type - Date

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import { ApolloServer, gql, MockList } from 'apollo-server'
+import { GraphQLScalarType, Kind } from 'graphql'
 import casual from 'casual'
 
 const books = [
@@ -13,6 +14,8 @@ const books = [
 ]
 
 const typeDefs = gql`
+  scalar Date
+
   type Book {
     title: String
     author: String
@@ -24,12 +27,28 @@ const typeDefs = gql`
   type Query {
     books: [Book]
     people: [Person]
+    drink: Drink
+  }
+  type Drink {
+    create: Date
   }
 `
 const resolvers = {
   Query: {
     books: () => books
-  }
+  },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date scalar type',
+    parseValue: value => new Date(value), // from client
+    serialize: value => value.getTime(), // sent to client
+    parseLiteral: ast => {
+      if (ast.kind === Kind.INT) {
+        return parseInt(ast.value, 10)
+      }
+      return null
+    }
+  })
 }
 
 const mocks = {
@@ -38,7 +57,10 @@ const mocks = {
     age: () => casual.integer(0, 120)
   }),
   Query: () => ({
-    people: () => new MockList([0, 12])
+    people: () => new MockList([0, 12]),
+    drink: () => ({
+      create: () => new Date()
+    })
   })
 }
 
@@ -49,5 +71,5 @@ const server = new ApolloServer({
 })
 
 server.listen().then(({ url }) => {
-  console.log('Server ready', url)
+  console.log('Server ready open', url)
 })
