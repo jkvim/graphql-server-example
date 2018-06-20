bbf23991418396a2ed9cea0023b4b81baa5f70e4
jkvim <jkvim@outlook.com>
2018-06-21 03:04:32 +0800

    add mutation

--- server.js
+++ server.js
@@ -3,25 +3,37 @@ const { ApolloServer, gql } = require('apollo-server');
 const books = [
   {
     title: 'Harry Potter',
-    author: 'J.K. Rowling'
+    author: 'J.K. Rowling',
+    like: 1,
   },
   {
     title: 'SICP',
-    author: 'Harold Abelson'
+    author: 'Harold Abelson',
+    like: 2,
   },
   {
     title: 'CSAPP',
-    author: 'Randal Bryant'
+    author: 'Randal Bryant',
+    like: 3,
   }
 ]
 
 const typeDefs = gql`
   type Book {
     title: String
-    author: String
+    author: String,
+    like: Int
   }
   type Query {
     getBook(title: String): Book
+  }
+  type Mutation {
+    likeBook(title: String!): Book,
+    addBook(book: BookInput!): [Book]
+  }
+  input BookInput {
+    title: String!,
+    author: String!
   }
 `
 const resolvers = {
@@ -30,6 +42,20 @@ const resolvers = {
       return books.find(book => book.title === title);
     }
   },
+  Mutation: {
+    likeBook: (_, { title }) => {
+      const book = books.find(book => book.title === title)
+      if (book) {
+        book.like += 1;
+        return book;
+      }
+    },
+    addBook: (_, { book }) => {
+      book.like = 0;
+      books.push(book)
+      return books
+    }
+  }
 }
 
 const server = new ApolloServer({
