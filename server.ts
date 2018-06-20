c118f1cb1baf8f07e8a41478ed06a516ffdb8cf7
jkvim <jkvim@outlook.com>
2018-06-21 00:17:17 +0800

    add hello world

--- server.ts
+++ server.ts
@@ -1,28 +1,13 @@
 import { ApolloServer, gql } from 'apollo-server'
 
-const books = [
-  {
-    title: 'Harry Potter and the Chamber of Secrets',
-    author: 'J.K. Rowling',
-  },
-  {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-  },
-]
-
 const typeDefs = gql`
-  type Book {
-    title: String,
-    author: String,
-  }
   type Query {
-    books: [Book]
+    hello: String,
   }
 `
 const resolvers = {
   Query: {
-    books: () => books,
+    hello: () => 'Hello World'
   }
 }
 
